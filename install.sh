3b6e2dba05033245a002649345b9d660b65f3b63
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-08-22 16:02:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo 'iptables -t nat -N DOCKER' /etc/csf/csfpost.sh
-echo 'iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER' /etc/csf/csfpost.sh
-echo 'iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER' /etc/csf/csfpost.sh
-echo 'iptables -t nat -A POSTROUTING -s 172.17.0.0/16 ! -o docker0 -j MASQUERADE' /etc/csf/csfpost.sh
-echo 'iptables -t filter -N DOCKER' /etc/csf/csfpost.sh
-echo 'iptables -t filter -A FORWARD -o docker0 -j DOCKER' /etc/csf/csfpost.sh
-echo 'iptables -t filter -A FORWARD -o docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT' /etc/csf/csfpost.sh
-echo 'iptables -t filter -A FORWARD -i docker0 ! -o docker0 -j ACCEPT' /etc/csf/csfpost.sh
-echo 'iptables -t filter -A FORWARD -i docker0 -o docker0 -j ACCEPT' /etc/csf/csfpost.sh
+echo 'iptables -t nat -N DOCKER' >> /etc/csf/csfpost.sh
+echo 'iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER' >> /etc/csf/csfpost.sh
+echo 'iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER' >> /etc/csf/csfpost.sh
+echo 'iptables -t nat -A POSTROUTING -s 172.17.0.0/16 ! -o docker0 -j MASQUERADE' >> /etc/csf/csfpost.sh
+echo 'iptables -t filter -N DOCKER' >> /etc/csf/csfpost.sh
+echo 'iptables -t filter -A FORWARD -o docker0 -j DOCKER' >> /etc/csf/csfpost.sh
+echo 'iptables -t filter -A FORWARD -o docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT' >> /etc/csf/csfpost.sh
+echo 'iptables -t filter -A FORWARD -i docker0 ! -o docker0 -j ACCEPT' >> /etc/csf/csfpost.sh
+echo 'iptables -t filter -A FORWARD -i docker0 -o docker0 -j ACCEPT' >> /etc/csf/csfpost.sh
 service docker restart
 csf -r
 
